4fe7edb88fa8bea05a858f9c05496690f65b1fa8
yeeun <remnantyeeun@gmail.com>
2026-02-23 12:47:45 +0900

    초기 페이지 구현

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="CarPricer")
+st.title("내 차의 진짜 가치,")
+st.title("CarPraicer가 알려드립니다")
---
+++ pages/1_가격예측.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.title("가격 예측 페이지")
